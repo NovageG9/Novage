4762f5f73225f9d933f48c435e4223aff7b402e9
yinc <yinchenwang@yinchendemacbook-pro.home>
2022-12-12 18:42:06 +0100

    upload

---
+++ assets/js/my.js
@@ -0,0 +1,11 @@
+
+function afterLogin(){
+    window.location.href = "index.html"
+  }
+
+function search(){
+    window.location.href = "services.html"
+}
+
+let as = document.getElementById("sss")
+as.addEventListener("click", search())
