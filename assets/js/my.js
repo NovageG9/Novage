254022c45c29a215d24ccce43a9682c876bb881c
yinc <yinchenwang@yinchendemacbook-pro.home>
2022-12-13 17:40:25 +0100

    ajouter

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,11 +1,3 @@
-
-function afterLogin(){
-    window.location.href = "index.html"
-  }
-
-function search(){
-    window.location.href = "services.html"
-}
-
-let as = document.getElementById("sss")
-as.addEventListener("click", search())
+document.getElementById('mapp').contentDocument.addEventListener('click', () => {
+	window.location.href = "service.html"
+})
